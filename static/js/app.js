c411888ae177a1f8d51940ed59f2fcb0ba4450d1
Alyssa <allym.moyer21@gmail.com>
2020-03-08 14:42:31 -0700

    updated app

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,13 @@
 // from data.js
 var tableData = data;
+function updateTable(dataSelect){
 
 //select the table body from index
 var tbody = d3.select("tbody");
 
 
 //loop through the data and add a row to the table for each
-tableData.forEach((ufo) => {
+dataSelect.forEach((ufo) => {
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(([key, value]) => {
       var cell = row.append("td");
@@ -14,19 +15,29 @@ tableData.forEach((ufo) => {
     });
   });
 
+};
+//creates table with all UFO data
+updateTable(tableData);
 
-  var button = d3.select("#filter-btn");
+//variable for the button on the page
+var button = d3.select("#filter-btn");
 
   //var userInput = d3.select("#form-control");
 
   //var inputValue = userInput.property("value");
 
  // console.log(inputValue);
-
-
+//  var table = d3.select('tbody');
+//  var rows = table.selectAll("tr");
+//  //console.log(rows);
+//  var td = rows.selectAll("td");
+//  //console.log(td);
+// Array.from(td).forEach((ind) => {
+//   console.log(ind);
+// });
   
 button.on("click", function() {
-
+    
     // Select the input element and get the raw HTML node
     var userInput = d3.select("#datetime");
   
@@ -34,9 +45,31 @@ button.on("click", function() {
     var inputValue = userInput.property("value");
   
     console.log(inputValue);
-    //console.log(people);
+    
   
     var dataSearch = tableData.filter(ufo => ufo.datetime === inputValue);
-  
-    console.log(dataSearch);
+    if (inputValue == null){
+      console.log("Please enter a date");
+      console.log(dataSearch);
+    }
+    else if (dataSearch == null) {
+      console.log("No entries for this date. Please make sure your date is valid")
+    } 
+    else {
+      console.log(dataSearch);
+       var tbody = d3.select("tbody");
+       var rows = tbody.selectAll("tr").remove();
+
+       dataSearch.forEach((ufo) => {
+         var row = tbody.append("tr");
+         Object.entries(ufo).forEach(([key, value]) => {
+         var cell = row.append("td");
+         cell.text(value);
+      });
+    });
+    }
+    
+   
+
 });
+
